bd97658f7abd824c22eb16a65d8c0835bd284545
davidperezmartorell <106980919+davidperezmartorell@users.noreply.github.com>
2024-06-22 00:57:05 +0200

    Create init.R

---
+++ init.R
@@ -0,0 +1,9 @@
+my_packages <- c("shiny", "shinyjs", "dplyr", "ggplot2", "leaflet", "leaflet.providers", "rnaturalearth", "rnaturalearthdata", "raster", "sf", "tidyverse", "terra", "rgbif", "DT", "reactable")
+
+install_if_missing <- function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
