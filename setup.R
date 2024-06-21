41ef8b2e7d6f42970b8d3eec9f5fd3741e6ca702
davidperezmartorell <106980919+davidperezmartorell@users.noreply.github.com>
2024-06-21 23:41:33 +0200

    Create setup.R

---
+++ setup.R
@@ -0,0 +1,29 @@
+# setup.R
+
+# Instalar paquetes necesarios
+install.packages(c(
+    "readr",
+    "pryr",
+    "dplyr",
+    "ggplot2",
+    "leaflet",
+    "leaflet.providers",
+    "shiny",
+    "shinyjs",
+    "rnaturalearth",
+    "rnaturalearthdata",
+    "raster",
+    "sf",
+    "tidyverse",
+    "terra",
+    "rgbif",
+    "DT",
+    "reactable",
+    "plotly",
+    "RColorBrewer",
+    "pdfetch",
+    "rmarkdown",
+    "htmlTable",
+    "webshot",
+    "pagedown"
+))
