41c31e6e9f6112d7686865738cb8b721cd0eaccb
davidperezmartorell <106980919+davidperezmartorell@users.noreply.github.com>
2024-06-22 01:50:42 +0200

    Update install.R

--- install.R
+++ install.R
@@ -1,31 +1,27 @@
-# List of packages to install
-packages <- c(
-  "readr", 
-  "pryr", 
-  "dplyr", 
-  "ggplot2", 
-  "leaflet", 
-  "leaflet.providers", 
-  "shiny", 
-  "shinyjs", 
-  "rnaturalearth", 
-  "rnaturalearthdata", 
-  "raster", 
-  "sf", 
-  "tidyverse", 
-  "terra", 
-  "rgbif", 
-  "DT", 
-  "reactable", 
-  "plotly", 
-  "RColorBrewer", 
-  "pdfetch", 
-  "rmarkdown", 
-  "htmlTable", 
-  "webshot", 
-  "pagedown"
-)
+install.packages("leaflet")
+install.packages("readr") 
+install.packages("pryr") 
+install.packages("dplyr") 
+install.packages("ggplot2") 
+install.packages("leaflet") 
+install.packages("leaflet.providers")
+install.packages("shiny") 
+install.packages("rnaturalearth") 
+install.packages("rnaturalearthdata" 
+install.packages("raster") 
+install.packages("sf") 
+install.packages("tidyverse")
+install.packages("terra") 
+install.packages("rgbif") 
+install.packages("DT") 
+install.packages("reactable")
+install.packages("plotly") 
+install.packages("RColorBrewer")
+install.packages("pdfetch")
+install.packages("rmarkdown")
+install.packages("htmlTable") 
+install.packages("webshot")
+install.packages("pagedown")
 
-# Install packages if not already installed
-install.packages(packages, repos = "https://cloud.r-project.org")
+
 
